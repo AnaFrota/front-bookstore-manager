c868a2caffd2049dea0a58e797363abfbcf14e0d
AnaFrota <anabeatriz.frotac@gmail.com>
2022-02-28 20:15:37 -0300

    putting global colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,4 +3,13 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#CC0000",
+        secondary: "#333",
+      },
+    },
+  },
+});
